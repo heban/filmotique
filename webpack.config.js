c065e73093cf70553b3a683a88f1bd14ce0e7d2c
heban <pawel.ochota@gmail.com>
2017-07-09 15:23:45 +0200

    Primary template for index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const webpack = require("webpack");
 const packageJson = require("./package.json");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackHarddiskPlugin = require("html-webpack-harddisk-plugin");
 const PATHS = {
     entryFile: path.resolve(__dirname, "./src/js/app.js"),
     bundleName: "js/bundle.js",
@@ -8,7 +10,20 @@ const PATHS = {
     publicPath: "./build/"
 };
 
-let plugins = [];
+let plugins = [
+    new HtmlWebpackPlugin({
+        template: "./src/index.ejs",
+        title: packageJson.name,
+        keywords: packageJson.keywords,
+        description: packageJson.description,
+        cache: false,
+        alwaysWriteToDisk: true
+    }),
+    new HtmlWebpackHarddiskPlugin({
+        outputPath: PATHS.publicPath
+    })
+];
+
 if (process.env.NODE_ENV === "production") {
     plugins.push(new webpack.optimize.UglifyJsPlugin());
 }
